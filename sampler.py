5a5d1f805b948d151e0972bb2c64ea34ffc396de
Rodrigo F. Díaz <rodrigo.diaz@unige.ch>
2016-02-10 21:23:20 +0100

    Working version. Thorough testing needed. Cyclic variables (like omega) missing.

--- sampler.py
+++ sampler.py
@@ -1,8 +1,9 @@
+from math import log
+
 import numpy as np
 import numpy.random
-from numpy.random import multivariate_normal as mvn
 import scipy
-from math import log
+from numpy.random import multivariate_normal as mvn
 
 
 class Sampler(object):
@@ -86,7 +87,7 @@ class MetropolisSampler(Sampler):
     """
 
     def __init__(self, dim, lnprobfn, lnprobargs, lnprobkwargs,
-                 verbose=False):
+                 proposalcov=None, verbose=False):
 
         # Super class call
         super(MetropolisSampler, self).__init__(dim, lnprobfn, lnprobargs,
@@ -94,35 +95,37 @@ class MetropolisSampler(Sampler):
 
         # Start attributes that change at runtime.
         # number of accepted steps
-        self.naccepted = 0
+        self.naccepted = np.zeros(0, dtype=int)
         self.last_lnprob = None
 
-        # Array to keep number of proposals per parameter and iteration
-        self.proposalrecord = np.empty((0, dim))
-        self.acceptancerecord = np.empty((0, dim))
-
-        # Array that contain moment of last proposal per parameter
-        self.lastproposal = np.zeros(dim)
-        # Array that contain moment of last acceptance per parameter
-        self.lastacceptance = np.zeros(dim)
-        # Array that contain moment of last proposal update per parameter
-        self.lastupdate = np.zeros(dim)
-
         # Proposal scale matrix
-        self.proposalcov = np.empty((0, dim, dim))
+        if proposalcov is not None:
+            self.proposalcov = proposalcov
+        else:
+            # randomly initialised
+            x = np.random.rand(dim)
+            kprop = x[:, None] + x[None, :]
+
+            # Add power in diagonal until matrix becomes positive definite.
+            # TO BE DONE
+            self.proposalcov = kprop
+
+        self.proposalcovrecord = np.array([self.proposalcov])
 
     def accept(self, lnprob1, proposal):
         # Set new lnprob as last lnprob
         self.last_lnprob = lnprob1
 
         # Add proposal step to chain
-        self.chain[self.nlinks + 1] = proposal
+        self._chain[self.nlinks + 1] = proposal
         self._lnprob[self.nlinks + 1] = lnprob1
-        self.naccepted += 1
+
+        # Add one to array with accepted steps
+        self.naccepted[self.nlinks + 1] = 1
 
     def reject(self):
         # Repeat previous link in chain.
-        self.chain[self.nlinks + 1] = self.chain[self.nlinks]
+        self._chain[self.nlinks + 1] = self.chain[self.nlinks]
         self._lnprob[self.nlinks + 1] = self.last_lnprob
 
     def make_proposal(self):
@@ -143,22 +146,19 @@ class MetropolisSampler(Sampler):
 
         if r > 0:
             self.accept(lnprob1, proposal)
-            self.nlinks += 1
             return 1
         elif log(numpy.random.rand()) < r:
             self.accept(lnprob1, proposal)
-            self.nlinks += 1
             return 1
         else:
             self.reject()
-            self.nlinks += 1
             return 0
 
 
 def compute_covariance(m):
     # Subtract mean from each parameter
     mean_p = np.mean(m, axis=0)
-    return np.cov(m - mean_p), mean_p
+    return np.cov(m - mean_p, rowvar=0), mean_p
 
 
 class AdaptivePCASampler(MetropolisSampler):
@@ -172,12 +172,13 @@ class AdaptivePCASampler(MetropolisSampler):
 
     :param int startpca: first iteration on which PCA is used.
     """
-    def __init__(self, dim, lnprobfn, lnprobargs, lnprobkwargs,
-                 startpca=10000, npca=100, verbose=False, **kwargs):
+    def __init__(self, dim, lnprobfn, lnprobfnargs, lnprobfnkwargs,
+                 proposalcov=None, startpca=10000, npca=100, verbose=False,
+                 **kwargs):
 
-        super(AdaptivePCASampler, self).__init__(dim, lnprobfn, lnprobargs,
-                                                 lnprobkwargs, verbose=verbose)
-
+        super(AdaptivePCASampler, self).__init__(dim, lnprobfn, lnprobfnargs,
+                                                 lnprobfnkwargs, proposalcov,
+                                                 verbose)
         self.startpca = startpca
         self.npca = npca
         self.nupdatepca = kwargs.pop('nupdatepca', np.inf)
@@ -185,36 +186,51 @@ class AdaptivePCASampler(MetropolisSampler):
         # Initialise change of basis matrix
         self.cobmatrix = np.empty((dim, dim))
 
-        self.pcaproposalrecord = np.empty((0, dim))
-        self.pcaacceptancerecord = np.empty((0, dim))
+        #
+        # Initialise arrays for bookkeeping
+        #
 
-        # Initialise proposal scale for principal components
-        self.pc_proposalscale = np.zeros((0, self.dim))
+        # Array to keep number of proposals per parameter and iteration
+        self.proposalrecord = np.zeros((1, dim))
 
-        # Initialise update interval per parameter to 100
-        self.update_interval = np.full(dim, 100)
+        # Array to keep number of acceptances per parameter and interation
+        self.acceptancerecord = np.zeros((1, dim))
+
+        # Bookkeeping of PCA proposals
+        # self.pcaproposalrecord = np.empty((0, dim))
+        # self.pcaacceptancerecord = np.empty((0, dim))
+
+        # Arrays to keep number of proposals and acceptance between updates.
+        self.nproposed_since_update = np.zeros(dim)
+        self.naccepted_since_update = np.zeros(dim)
+
+        # Initialise update interval per parameter
+        self.update_interval = np.full(dim, 2)
         self.last_scalefactor = np.full(dim, np.nan)
 
-    def bookkeeping(self, indexvector, action):
-        if action == 'proposal':
-            # Keer record of number of proposals
-            self.proposalrecord = np.vstack((self.proposalrecord,
-                                             self.proposalrecord[-1] +
-                                             indexvector))
-            # Record moment of proposal
-            self.lastproposal[indexvector != 0] = indexvector * self.nlinks
+    def bookkeeping(self, indexvector, accept):
 
-        elif action == 'acceptance':
+        i = self.nlinks
+
+        # For all iterations keep record of number of proposals
+        self.proposalrecord[i] = self.proposalrecord[i-1] + indexvector
+
+        # Record proposal covariance for this step (even if identical)
+        self.proposalcovrecord[i] = self.proposalcov
+
+        #  Increase number of proposed steps since update per parameter
+        self.nproposed_since_update += indexvector
+
+        if accept:
             # Keep record of number of acceptances
-            self.acceptancerecord = np.vstack((self.acceptancerecord,
-                                               self.acceptancerecord[-1] +
-                                               indexvector))
-            # Record moment of acceptance
-            self.lastacceptance[indexvector != 0] = indexvector * self.nlinks
+            self.acceptancerecord[i] = self.acceptancerecord[i-1] + indexvector
 
-        elif action == 'update':
-            # Record moment of update
-            self.lastupdate[indexvector != 0] = indexvector * self.nlinks
+            # Increase number of accepted steps since update
+            self.naccepted_since_update += indexvector
+
+        else:
+            # Keep record of number of acceptances (in this case, just copy)
+            self.acceptancerecord[i] = self.acceptancerecord[i-1]
 
     def make_proposal(self, onebyone=True):
         """
@@ -224,66 +240,44 @@ class AdaptivePCASampler(MetropolisSampler):
         parameters at the same time or one by one.
         :return:
         """
+        if onebyone:
+            # Jump one parameter at a time. Choose randomly
+            jumpind = np.random.randint(0, self.dim)
+
+            # Array indicating which parameter jumps
+            self.jump = np.zeros(self.dim, dtype=int)
+            self.jump[jumpind] += 1
+
+            # Get scale from proposal covariance
+            jumpscale = np.diag(self.proposalcov)
+
+            # Proposed jump (for single parameter, zero for the rest)
+            proposedjump = self.jump * np.random.randn() * jumpscale
+
+        else:
+            # Jump all parameters
+            self.jump = np.ones(self.dim, dtype=int)
+
+            # Proposed jump
+            proposedjump = mvn(mean=np.zeros(self.dim), cov=self.proposalcov)
+
         # Proposing without PCA
         if self.nlinks < self.startpca:
-            if onebyone:
-                # Jump one parameter at a time. Choose randomly
-                jumpind = np.random.randint(0, self.dim)
+            # Starting point is last point in chain
+            startingpoint = self.chain[self.nlinks]
 
-                # Array indicating which parameter jumps
-                self.jump = np.zeros(self.dim, dtype=int)
-                self.jump[jumpind] += 1
-
-                # Keep records
-                self.bookkeeping(self.jump, action='proposal')
-
-                # Get scale from proposal covariance
-                jumpscale = np.diag(self.proposalcov[-1])[jumpind]
-
-                return self.chain[self.nlinks] + self.jump * np.random.randn() \
-                    * jumpscale
-
-            else:
-                self.jump = np.ones(self.dim, dtype=int)
-                self.bookkeeping(self.jump, action='proposal')
-
-                # Jump all parameters using covariance matrix
-                return mvn(mean=self.chain[self.nlinks],
-                           cov=self.proposalcov[-1])
-
-        # Proposing with PCA
+        # Propsing with PCA
         else:
             # Make proposal in rotated parameter space.
             # Convert last chain link to principal component space
-            pclast = np.dot(self.cobmatrix, self.chain[self.nlinks])
+            startingpoint = np.dot(self.cobmatrix, self.chain[self.nlinks])
 
-            if onebyone:
-                # Jump one parameter at a time. Choose randomly
-                jumpind = np.random.randint(0, self.dim)
+        newpoint = startingpoint + proposedjump
 
-                # Array indicating which parameter jumps
-                self.jump = np.zeros(self.dim, dtype=int)
-                self.jump[jumpind] += 1
-
-                # Keep records
-                self.bookkeeping(self.jump, action='PCAproposal')
-
-                # Get scale from proposal covariance
-                jumpscale = self.pc_proposalscale[-1][jumpind]
-
-                # New proposal in rotated space
-                pcprop = pclast + self.jump * np.random.randn() * jumpscale
-
-            else:
-                self.jump = np.ones(self.dim)
-                self.bookkeeping(self.jump, action='PCAproposal')
-
-                # Jump all parameters at the same time.
-                pcprop = pclast + np.random.rand(self.dim) * \
-                    self.pc_proposalscale[-1]
-
-            # Go back to regular parameter space
-            return np.dot(self.cobmatrix.T, pcprop)
+        if self.nlinks < self.startpca:
+            return newpoint
+        else:
+            return np.dot(self.cobmatrix.T, newpoint)
 
     def pca(self):
         # Update proposal scales and PCA matrix.
@@ -303,8 +297,12 @@ class AdaptivePCASampler(MetropolisSampler):
         self.cobmatrix = eigvec.T
 
         # Use eigenvalues as proposed parameter scale for pc's
-        self.pc_proposalscale = np.concatenate((self.pc_proposalscale,
-                                                np.abs(eigval)), axis=0)
+        self.proposalcov = np.diag(np.abs(eigval))
+
+        # Reset counters of updates
+        self.nproposed_since_update[self.jump != 0] = 0
+        self.naccepted_since_update[self.jump != 0] = 0
+
         return
 
     def update_proposal_scale(self, target_rate=0.25):
@@ -319,17 +317,16 @@ class AdaptivePCASampler(MetropolisSampler):
         # If the squared difference (acceptance_rate - psi0)**2 is larger
         # than a few times this value, then the scale must be updated
         update_stat = self.update_interval * target_rate * (1 - target_rate) / \
-            (self.nlinks - self.lastupdate)
+            self.nproposed_since_update
 
         # Compute acceptance rate
-        naccepted_since_update = self.acceptancerecord[-1] - \
-            self.acceptancerecord[self.lastupdate]
-        nproposed_since_update = self.proposalrecord[-1] - \
-            self.proposalrecord[self.lastupdate]
-        acceptance_rate = naccepted_since_update / nproposed_since_update
+        acceptance_rate = np.where(self.nproposed_since_update > 0,
+                                   self.naccepted_since_update /
+                                   self.nproposed_since_update,
+                                   target_rate)
 
         # condition for updating
-        condupdate = (acceptance_rate - target_rate) ** 2 > update_stat
+        condupdate = ((acceptance_rate - target_rate) ** 2 > update_stat) * (self.nproposed_since_update > 100)
 
         # If no parameter needs updating, exit.
         if np.all(~condupdate):
@@ -341,11 +338,11 @@ class AdaptivePCASampler(MetropolisSampler):
         k = np.full(self.dim, 0.01)
 
         # Compute factoring scales for different ranges of ratio rho
-        k[rho > 2] = rho**2.0
-        k[1 < rho <= 2] = rho
-        k[0.5 < rho <= 1] = np.sqrt(rho)
-        k[0.2 < rho <= 0.5*rho] = rho**1.5
-        k[0.1 < rho <= 0.2*rho] = rho**2.0
+        k[rho > 2] = rho[rho > 2]**2.0
+        k[(rho > 1) * (rho <= 2)] = rho[(rho > 1) * (rho <= 2)]
+        k[(rho > 0.5) * (rho <= 1)] = np.sqrt(rho[(rho > 0.5) * (rho <= 1)])
+        k[(rho > 0.2) * (rho <= 0.5)] = rho[(rho > 0.2) * (rho <= 0.5)]**1.5
+        k[(rho > 0.1) * (rho <= 0.2)] = rho[(rho > 0.1) * (rho <= 0.2)]**2
 
         # Set all parameters where updating is not necessary to one
         k[~condupdate] = 1.0
@@ -356,30 +353,57 @@ class AdaptivePCASampler(MetropolisSampler):
         self.update_interval[cond * ~np.isnan(self.last_scalefactor)] += 1
 
         # Update proposal scale
-        newpropsalscale = self.proposalcov[-1].copy()
-        np.fill_diagonal(newpropsalscale, np.diag(self.proposalcov) * k)
-        self.proposalcov = np.vstack((self.proposalcov, newpropsalscale))
+        newpropsalcov = self.proposalcov.copy()
+        newscale = np.diag(self.proposalcov) * k
+        newscale = np.where(newscale != 0, newscale,
+                            self.chain[self.nlinks] * 0.1)
+
+        newpropsalcov[np.diag_indices_from(newpropsalcov)] = newscale
+
+        self.proposalcov = newpropsalcov
 
         self.last_scalefactor = k
 
-        # Bookkeeping of updates.
-        self.bookkeeping(condupdate.astype(int), action='update')
+        # Reset counters of updates
+        self.nproposed_since_update[self.jump != 0] = 0
+        self.naccepted_since_update[self.jump != 0] = 0
 
         """
         if self.verbose:
-            print('Scale adjusted for parameter {0};\t\t acceptence rate: '
-                  '{1:.2f}%; old scale {2:.2e}; new scale {3:.2e}; '
-                  'Value = {4:.3f}'.format(par.label, acceptance_rate * 100,
-                                           betamu, betamu * k, par.get_value())
-                )
-        else:
-            print('Scale adjusted for parameter {0};\t\t acceptence rate: '
-                  '{1:.2f}%; old scale {2:.6f}; new scale {3:.6f}; '
-                  'Value = {4:.3f}'.format(par.label, acceptance_rate*100,
-                                           betamu, betamu*k, par.get_value())
-                  )
+            for j, upbool in enumerate(condupdate):
+                if upbool:
+                    print('Scale adjusted for parameter {}; old scale: {:.2e}; '
+                          'new scale: {:.2e}'
+                          ''.format(j, np.diag(self.proposalcovrecord[-2])[j],
+                                    np.diag(newpropsalcov)[j]))
         """
         return
+
+    def initialise_arrays(self, n):
+
+        # Add n empty elements to chain.
+        self._chain = np.concatenate((self._chain, np.empty((n, self.dim))),
+                                     axis=0)
+
+        self._lnprob = np.concatenate((self._lnprob, np.empty(n)), axis=0)
+
+        # Add n elements to bookkeeping arrays
+        self.naccepted = np.concatenate((self.naccepted, np.zeros(n, dtype=int))
+                                        )
+
+        self.proposalrecord = np.concatenate((self.proposalrecord,
+                                              np.empty((n, self.dim), dtype=int)
+                                              ))
+
+        self.acceptancerecord = np.concatenate((self.acceptancerecord,
+                                                np.empty((n, self.dim),
+                                                         dtype=int)
+                                                ))
+
+        self.proposalcovrecord = np.concatenate((self.proposalcovrecord,
+                                                 np.empty((n, self.dim,
+                                                           self.dim))),
+                                                axis=0)
 
     def run_mcmc(self, n, p0=None, lnprob0=None):
         """
@@ -400,34 +424,44 @@ class AdaptivePCASampler(MetropolisSampler):
         # in chain.
         self.nlinks = self._chain.shape[0]
 
-        # Add n empty elements to chain.
-        self._chain = np.concatenate((self._chain, np.empty(n, self.dim)), axis=0)
-
-        self._lnprob = np.concatenate((self._lnprob, np.empty(n)), axis=0)
+        self.initialise_arrays(n)
 
         # If fresh run, try to compute lnprob at starting point.
         if self.nlinks == 0:
+            print('Fresh Markov Chain. Adding first link using starting point.')
             try:
                 self.last_lnprob = self.get_lnprob(p0)
             except:
                 raise AttributeError('First evaluation of target distribution'
                                      'produced and error.')
 
+            # Initialise chain to starting point
+            self._chain[self.nlinks] = p0
+            self._lnprob[self.nlinks] = self.last_lnprob
+            n -= 1
+
+        else:
+            self.nlinks -= 1
+
         for i in xrange(n):
+
+            if self.verbose and i % 500 == 0:
+                print('#### Iteration {} out of {}'.format(i, n))
+
+            # Update PCA matrix every nupdatepca iterations.
+            if self.nlinks >= self.startpca and \
+               (self.nlinks - self.startpca) % self.nupdatepca == 0:
+                self.pca()
 
             # Sample
             acceptflag = self.sample()
 
-            if acceptflag:
-                self.bookkeeping(self.jump, action='acceptance')
+            # Increase number of links in chain
+            self.nlinks += 1
+            self.bookkeeping(self.jump, accept=acceptflag)
 
             # Update proposal scales
             self.update_proposal_scale(0.25)
-
-            # Update PCA matrix every nupdatepca iterations.
-            if self.nlinks > self.startpca and \
-               (self.nlinks - self.startpca) % self.nupdatepca == 0:
-                self.pca()
 
         return
 
