45bf1822cc8cc7f6923aec5d7ad889c243427867
Rodrigo F. Díaz <rodrigo.diaz@unige.ch>
2016-05-05 23:59:19 +0200

    Slight modification to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from distutils.core import setup
 
 setup(name='cobmcmc',
-      description='Change-of-Basis, a flexible Metropolis-Hastings MCMC algorithm.',
+      description='Change-of-Basis, '
+                  'a flexible Metropolis-Hastings MCMC algorithm.',
       version='0.1.0',
       author='Rodrigo F. Diaz',
       author_email='rodrigo.diaz@unige.ch',
       url='',
+      long_description='**COMING SOON**',
       packages=['cobmcmc'],
       requires=['numpy', 'scipy']
       )
